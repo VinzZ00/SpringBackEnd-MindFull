3dd2f4052dbda5ecdd48de61dd128641bdb43348
Elvin <vinzln1209@gmail.com>
2024-02-03 11:22:39 +0700

    adding delete Step service into service module

--- src/main/java/com/minidfull/backend/repository/StepRepository.java
+++ src/main/java/com/minidfull/backend/repository/StepRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface StepRepository extends JpaRepository<Steps, Long> {
     Optional<Steps> findByStepId(Long stepId);
     boolean deleteByStepId(Long stepId);
+    boolean existsByGoal(Long goalId);
 }
--- src/main/java/com/minidfull/backend/services/stepService/DeleteStepService.java
+++ src/main/java/com/minidfull/backend/services/stepService/DeleteStepService.java
@@ -1,23 +1,61 @@
 package com.minidfull.backend.services.stepService;
 
+import com.minidfull.backend.dto.BackEndException;
 import com.minidfull.backend.dto.stepDtos.DeleteStepsDTO;
+import com.minidfull.backend.entity.Steps;
+import com.minidfull.backend.repository.GoalRepository;
 import com.minidfull.backend.repository.StepRepository;
 import jakarta.transaction.Transactional;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Validator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 
 @Service
 public class DeleteStepService {
 
-    @Autowired private StepRepository repo;
+    @Autowired private StepRepository stepRepo;
+    @Autowired private GoalRepository goalRepo;
     @Autowired private Validator validator;
 
     @Transactional
     public void deleteSteps(DeleteStepsDTO steps) {
-        if 
-    }
+         Set<ConstraintViolation<DeleteStepsDTO>> violations = validator.validate(steps);
 
+         if (!violations.isEmpty()) {
+             throw new ConstraintViolationException(violations);
+         }
+
+         Set<Long> goalIds = Arrays
+                 .stream(steps.getStepIds())
+                 .collect(Collectors.toSet());
+
+         for (Long s : steps.getStepIds()) {
+             if(stepRepo.existsById(s)) {
+                 if (!stepRepo.deleteByStepId(s)) {
+                     throw BackEndException.SpringServerLogicalError(this.getClass().getName());
+                 }
+             } else {
+                 throw BackEndException.notFoundException(Steps.class.getName());
+             }
+         }
+
+        for (Long g : goalIds) {
+            if(goalRepo.existsByGoalId(g)) {
+                if (!stepRepo.existsByGoal(g)) {
+                    if (!goalRepo.deleteGoalsByGoalId(g)) {
+                        throw BackEndException.SpringServerLogicalError(this.getClass().getName());
+                    }
+                }
+            } else {
+                throw BackEndException.SpringServerLogicalError(this.getClass().getName());
+            }
+        }
+    }
 }
